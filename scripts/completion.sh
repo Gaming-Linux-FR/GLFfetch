b58c8f954f8ef075668ef87b36f8e6d41dedbb14
Sparkway <84066435+Sparkway@users.noreply.github.com>
2024-01-16 22:00:25 +0100

    Update completion.sh

--- scripts/completion.sh
+++ scripts/completion.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 source ~/.config/fastfetch/GLFfetch/scripts/vars.sh
 
-if [[ $percentage -le 100 ]]
+if [[ $percentage -lt 100 ]]
 then
   ProgressBar ${install_time} ${end_challenge}
 else [[ $percentage -ge 100 ]]
-  percentage=100
   echo "GLF Challenge completed"
 fi
