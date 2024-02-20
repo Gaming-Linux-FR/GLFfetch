c0ae9e782f2bfe52acaa95438c3b70608f2353cf
Sparkway <84066435+Sparkway@users.noreply.github.com>
2024-02-20 22:09:49 +0100

    Update icon.sh

--- scripts/icon.sh
+++ scripts/icon.sh
@@ -4,7 +4,7 @@ os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2 | tr -d \")
 old_icon=$(grep key "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 old_color=$(grep keyColor "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 
-if [[ "$os" == "arch " ]]; then
+if [[ "$os" == "arch" ]]; then
   new_icon="󰣇"
   new_color="bright_blue"
 elif [[ "$os" == "endeavouros" ]]; then
