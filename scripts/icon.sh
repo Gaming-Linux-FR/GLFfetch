bd30b21e83368cdec89671e070f3dbbfeb9e9fa2
babilinx <babilinx.evx1o@simplelogin.com>
2024-02-24 16:01:49 +0100

    Use 'case' statement in icon.sh

--- scripts/icon.sh
+++ scripts/icon.sh
@@ -4,31 +4,32 @@ os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2 | tr -d \")
 old_icon=$(grep key "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 old_color=$(grep keyColor "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 
-if [[ "$os" == "arch" ]]; then
-  new_icon="󰣇"
-  new_color="bright_blue"
-elif [[ "$os" == "endeavouros" ]]; then
-  new_icon=""
-  new_color="bright_magenta"
-elif [[ "$os" == "manjaro" ]]; then
-  new_icon="󱘊"
-  new_color="bright_green"
-elif [[ "$os" == "debian" ]]; then
-  new_icon="󰣚"
-  new_color="bright_red"
-elif [[ "$os" == "ubuntu" ]]; then
-  new_icon="󰕈"
-  new_color="red"
-elif [[ "$os" == "fedora" ]]; then
-  new_icon="󰣛"
-  new_color="blue"
-elif [[ "$os" == "opensuse" ]]; then
-  new_icon=""
-  new_color="green"
-else
-  new_icon="󰣇"
-  new_color="bright_blue"
-fi
+switch "$os" in
+  "arch")
+    new_icon="󰣇"
+    new_color="bright_blue" ;;
+  "endeavouros")
+    new_icon=""
+    new_color="bright_magenta" ;;
+  "manjaro")
+    new_icon="󱘊"
+    new_color="bright_green" ;;
+  "debian")
+    new_icon="󰣚"
+    new_color="bright_red" ;;
+  "ubuntu")
+    new_icon="󰕈"
+    new_color="red" ;;
+  "fedora")
+    new_icon="󰣛"
+    new_color="blue" ;;
+  "opensuse")
+    new_icon=""
+    new_color="green" ;;
+  *)
+    new_icon="󰣇"
+    new_color="bright_blue" ;;
+esac
 
 if [[ -n "$new_icon" ]]; then
   sed -zi -e "s/$old_icon/$new_icon/" -e "s/$old_color/$new_color/" "$file"
