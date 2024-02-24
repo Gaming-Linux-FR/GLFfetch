d4b0fc616003177138c245638346fd07bb6e1119
Babilinx <babilinx.evx1o@simplelogin.com>
2024-02-24 15:12:07 +0000

    Replace 'switch' with 'case'

--- scripts/icon.sh
+++ scripts/icon.sh
@@ -4,7 +4,7 @@ os=$(grep ^ID= /etc/os-release | cut -d "=" -f 2 | tr -d \")
 old_icon=$(grep key "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 old_color=$(grep keyColor "$HOME"/.config/fastfetch/GLFfetch/challenge.jsonc | head -n 1 | cut -d ":" -f2 | tr -d \", | awk '{$1=$1};1')
 
-switch "$os" in
+case "$os" in
   "arch")
     new_icon="󰣇"
     new_color="bright_blue" ;;
